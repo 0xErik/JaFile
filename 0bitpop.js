523e37da7a1d7074182c9d440ad90cfb9899fc6b
0xErik <eriklaji7@gmail.com>
2025-08-03 22:02:44 +0600

    Create 0bitpop.js

---
+++ 0bitpop.js
@@ -0,0 +1,5 @@
+<script type="text/javascript" data-cfasync="false">
+/*<![CDATA[/* */
+(function(){var d=window,s="ea76a004bc6ac69c1d682539914865d0",b=[["siteId",340+861-236*710+5388929],["minBid",0],["popundersPerIP","6:1"],["delayBetween",5],["default",false],["defaultPerDay",0],["topmostLayer","auto"]],t=["d3d3LmJldHRlcmFkc3lzdGVtLmNvbS9IQUhSVFEvcFVaUVAvaXNob3VsZC5taW4uanM=","ZDJrazBvM2ZyN2VkMDEuY2xvdWRmcm9udC5uZXQvenBlZXJqcy5taW4uanM="],f=-1,k,e,q=function(){clearTimeout(e);f++;if(t[f]&&!(1780156908000<(new Date).getTime()&&1<f)){k=d.document.createElement("script");k.type="text/javascript";k.async=!0;var w=d.document.getElementsByTagName("script")[0];k.src="https://"+atob(t[f]);k.crossOrigin="anonymous";k.onerror=q;k.onload=function(){clearTimeout(e);d[s.slice(0,16)+s.slice(0,16)]||q()};e=setTimeout(q,5E3);w.parentNode.insertBefore(k,w)}};if(!d[s]){try{Object.freeze(d[s]=b)}catch(e){}q()}})();
+/*]]>/* */
+</script>
